6374dd803bde760d58c37f8eca65b33100189dd3
Rajat Wankhede <64713729+RajatWankhede14@users.noreply.github.com>
2023-03-11 23:44:27 +0530

    added gitignore to sanity/node_modules

---
+++ sanity/schemas/comment.ts
@@ -0,0 +1,40 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'comment',
+  title: 'Comment',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'approved',
+      title: 'Approved',
+      type: 'boolean',
+      description: "Comments don't show on the site without approval",
+    }),
+    defineField({
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+    }),
+    defineField({
+      name: 'comment',
+      title: 'Comment',
+      type: 'text',
+    }),
+    defineField({
+      name: 'post',
+      title: 'Post',
+      type: 'reference',
+      to: [
+        {
+          type: 'post',
+        },
+      ],
+    }),
+  ],
+})
