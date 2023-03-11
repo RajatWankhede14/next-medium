72e7560c7b56e18d2c811deb39ca790f751bdf0a
Rajat Wankhede <64713729+RajatWankhede14@users.noreply.github.com>
2023-03-12 00:13:54 +0530

    added changes

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+import {defineCliConfig} from '@sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: 'lwr32lso',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
